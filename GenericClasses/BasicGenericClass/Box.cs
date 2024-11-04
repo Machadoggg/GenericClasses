bed97b43ffcfe50c2ff3f8b97828cf1accf59349
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-04 08:11:43 -0500

    Add project files.

---
+++ GenericClasses/BasicGenericClass/Box.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericClasses.BasicGenericClass
+{
+    public class Box<T>
+    {
+        public T Content { get; set; }
+
+        public Box(T content)
+        {
+            Content = content;
+        }
+
+        public void DisplayContent()
+        {
+            Console.WriteLine($"Content: {Content}");
+        }
+    }
+}
---
+++ GenericClasses/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using GenericClasses.BasicGenericClass;
+
+
+
+//BasicGenericClass
+var intBox = new Box<int>(123);
+intBox.DisplayContent();
+
+var stringBox = new Box<string>("Hello");
+stringBox.DisplayContent();
