b6eb466fb0ab281e4cf0dfc8931599328671ac0a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-04 13:29:58 -0500

    Generics with Comparison (Ordering Method)

---
+++ GenericClasses/GenericsWithComparison/Sorter.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericClasses.GenericsWithComparison
+{
+    public class Sorter<T> where T : IComparable<T>
+    {
+        public void Sort(List<T> list)
+        {
+            list.Sort();
+        }
+    }
+}
--- GenericClasses/Program.cs
+++ GenericClasses/Program.cs
@@ -4,10 +4,11 @@ using GenericClasses.GenericBasicClass;
 using GenericClasses.GenericsWithRestrictions;
 using GenericClasses.GenericWithInterfaces;
 using GenericClasses.GenericsWithDelegates;
+using GenericClasses.GenericsWithComparison;
 
 
 
-//Basic Generic Class
+//1: Basic Generic Class
 //permite almacenar datos de cualquier tipo.
 var intBox = new Box<int>(123);
 intBox.DisplayContent();
@@ -16,7 +17,7 @@ var stringBox = new Box<string>("Hello");
 stringBox.DisplayContent();
 
 
-// Generics with Restrictions
+//2: Generics with Restrictions
 //restringe el tipo genérico a ciertos tipos, como clases específicas o interfaces.
 var repo = new Repository<string>();
 repo.Add("Item 1");
@@ -29,7 +30,7 @@ foreach (var item in repo.GetAll())
 
 
 
-// Genéricos con Interfaces
+//3: Genéricos con Interfaces
 // Los genéricos también pueden implementar interfaces para mayor flexibilidad en su uso.Uso:
 IRepository<int> intRepo = new MemoryRepository<int>();
 intRepo.Add(10);
@@ -38,7 +39,7 @@ Console.WriteLine(intRepo.Get(0));
 Console.WriteLine(intRepo.Get(1));
 
 
-//Genéricos en Métodos
+//4: Genéricos en Métodos
 //Los métodos dentro de una clase pueden ser genéricos sin que toda la clase sea genérica.
 Utility.Print(42);
 Utility.Print(8.4);
@@ -46,10 +47,24 @@ Utility.Print("Generics in C#");
 
 
 
-//Genéricos con Delegados
+//5: Genéricos con Delegados
 //Podemos usar genéricos para crear delegados que acepten cualquier tipo.
 Transformer<int> squareInt = TransformerDemo.Square;
 Console.WriteLine(squareInt(5)); // Output: 25
 
 Transformer<double> squareDouble = TransformerDemo.Square;
 Console.WriteLine(squareDouble(3.5)); // Output: 12.25
+
+
+
+//6: Genéricos con Comparación (Método de Ordenamiento)
+//Las clases genéricas también se pueden usar para crear algoritmos reutilizables, 
+//como métodos de ordenamiento.
+var numbers = new List<int> { 3, 1, 4, 1, 5, 9 };
+var sorter = new Sorter<int>();
+sorter.Sort(numbers);
+
+foreach (var number in numbers)
+{
+    Console.WriteLine($"{number}");
+}
