81529303e9e251e831ef61a33876414019cd18c9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-11-04 12:25:45 -0500

    Generics classes with Restrictions

---
+++ GenericClasses/GenericsWithRestrictions/Repository.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericClasses.GenericsWithRestrictions
+{
+    public class Repository<T> where T : class
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public List<T> GetAll()
+        {
+            return _items;
+        }
+    }
+}
--- GenericClasses/Program.cs
+++ GenericClasses/Program.cs
@@ -1,11 +1,25 @@
 ﻿
 using GenericClasses.BasicGenericClass;
+using GenericClasses.GenericsWithRestrictions;
 
 
 
-//BasicGenericClass
+//Basic Generic Class
+//permite almacenar datos de cualquier tipo.
 var intBox = new Box<int>(123);
 intBox.DisplayContent();
 
 var stringBox = new Box<string>("Hello");
 stringBox.DisplayContent();
+
+
+// Generics with Restrictions
+//restringe el tipo genérico a ciertos tipos, como clases específicas o interfaces.
+var repo = new Repository<string>();
+repo.Add("Item 1");
+repo.Add("Item 2");
+
+foreach (var item in repo.GetAll())
+{
+    Console.WriteLine(item);
+}
